8841507bc2adffe0beb162c596989badb2c5fbff
jeffgrunewald <jeff@grunewalddesign.com>
2017-08-01 09:24:07 -0400

    Refactoring a little

--- babysitter/lib/babysitter.ex
+++ babysitter/lib/babysitter.ex
@@ -1,10 +1,17 @@
 defmodule Babysitter do
 
   def tally_earnings(start_time, end_time) do
-    if end_time < start_time do
-        ((12 - start_time) + end_time) * 12
-    else
-        (end_time - start_time) * 12
-    end
+      calc_time_diff(start_time, end_time) |> mult_rate()
   end
+
+  defp calc_time_diff(start_time, end_time) do
+      if end_time < start_time do
+          ((12 - start_time) + end_time)
+      else
+          (end_time - start_time)
+      end
+  end
+
+  defp mult_rate(hours), do: hours * 12
+
 end

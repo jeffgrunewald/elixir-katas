36c77aa72346cb4ec78caec103c56af778f3886f
jeffgrunewald <jeff@grunewalddesign.com>
2017-08-17 13:52:29 -0400

    Completed the babysitter kata, perhaps sans some additional refactors

--- babysitter/lib/babysitter.ex
+++ babysitter/lib/babysitter.ex
@@ -1,8 +1,22 @@
 defmodule Babysitter do
+  @time_bound_start 5
+  @time_bound_midnight 12
+  @rate_postmidnight 16
+  @rate_prebed 12
+  @rate_premidnight 8
 
-  def tally_earnings(start_time, end_time, bed_time), do: (end_time - start_time) * calc_rate(start_time, end_time, bed_time)
+  def tally_earnings(start_time, end_time, bed_time) do      
+      exclusive_range(transform_time(start_time), transform_time(end_time))
+      |> Enum.map(fn hour -> calc_rate(hour, transform_time(bed_time)) end)
+      |> Enum.sum
+  end
 
-  defp calc_rate(start_time, end_time, bed_time) when end_time <= bed_time and end_time > 5, do: 12
-  defp calc_rate(start_time, end_time, bed_time) when end_time > bed_time, do: 8
-  defp calc_rate(start_time, end_time, bed_time) when end_time < 5, do: 16
+  defp calc_rate(hour, _bed_time) when hour >= @time_bound_midnight, do: @rate_postmidnight
+  defp calc_rate(hour, bed_time) when hour < bed_time, do: @rate_prebed
+  defp calc_rate(hour, bed_time) when hour >= bed_time and hour <= @time_bound_midnight, do: @rate_premidnight
+
+  defp transform_time(time) when time < @time_bound_start, do: time + @time_bound_midnight
+  defp transform_time(time), do: time
+
+  defp exclusive_range(start, stop), do: start..stop - 1
 end
--- babysitter/test/babysitter_test.exs
+++ babysitter/test/babysitter_test.exs
@@ -18,4 +18,20 @@ defmodule BabysitterTest do
       assert 16 == tally_earnings(1, 2, 8)
   end
 
+  test "one hour starting at midnight" do
+      assert 16 == tally_earnings(12, 1, 8)
+  end
+
+  test "bed time after midnight for one hour" do
+      assert 16 == tally_earnings(1, 2, 2)
+  end
+
+  test "time crosses bedtime" do
+      assert 20 == tally_earnings(7, 9, 8)
+  end
+
+  test "time crosses all three pay bands" do
+      assert 60 == tally_earnings(7,1,8)
+  end
+
 end

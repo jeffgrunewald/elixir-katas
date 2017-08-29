846cb9ef02367912c7e8c57ea4242fa340ef62a7
jeffgrunewald <jeff@grunewalddesign.com>
2017-08-29 13:00:12 -0400

    Second test, arbitrary inputs, now failing

--- bowling_game/lib/bowling_game.ex
+++ bowling_game/lib/bowling_game.ex
@@ -1,5 +1,7 @@
 defmodule BowlingGame do
+
   def get_score(string) do
       4
   end
+
 end
--- bowling_game/test/bowling_game_test.exs
+++ bowling_game/test/bowling_game_test.exs
@@ -2,7 +2,13 @@ defmodule BowlingGameTest do
   use ExUnit.Case
   import BowlingGame
 
-  test "returns the value of a single roll" do
+  test("returns the value of a single roll") do
       assert 4 == get_score("4")
+  end
+
+  test("returns an arbitrary value of a single roll") do
+      (1..9) |> Enum.each(fn x ->
+          assert x == get_score(to_string x)
+      end)
   end
 end

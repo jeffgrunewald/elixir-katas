8f3d4d6e3a874700eb49c4377956718d7e7dfed1
jeffgrunewald <jeff@grunewalddesign.com>
2017-08-31 14:11:49 -0400

    Passing arbitrary number roll numerals (or gutters)

--- bowling_game/lib/bowling_game.ex
+++ bowling_game/lib/bowling_game.ex
@@ -1,6 +1,13 @@
 defmodule BowlingGame do
 
   def get_score(string) do
+      String.codepoints(string)
+      |> Stream.map(&single_roll/1)
+      |> Enum.sum
+  end
+
+  defp single_roll("-"), do: 0
+  defp single_roll(string) do
       String.to_integer(string)
   end
 
--- bowling_game/test/bowling_game_test.exs
+++ bowling_game/test/bowling_game_test.exs
@@ -11,4 +11,12 @@ defmodule BowlingGameTest do
           assert x == get_score(to_string x)
       end)
   end
+
+  test("return 0 when a '-' is supplied") do
+      assert 0 == get_score("-")
+  end
+
+  test("returns sum of two when two rolls are passed") do
+      assert 9 == get_score("45")
+  end
 end

caec12145b9a8fa5e7c9e129a37b264c4cda5c8e
jeffgrunewald <jeff@grunewalddesign.com>
2017-10-07 23:41:20 -0400

    adding test setup context

--- hello_exunit/test/hello_exunit_test.exs
+++ hello_exunit/test/hello_exunit_test.exs
@@ -2,12 +2,17 @@ defmodule HelloExunitTest do
   use ExUnit.Case
   import HelloExunit.PMap
 
-  test "pmap maps a list" do
-    assert [2,4,6] = pmap([1,2,3], fn x -> x * 2 end)
+  setup _context do
+    {:ok, [in_list: [1,2,3],
+           out_list: [2,4,6]]}
   end
 
-  test "pmap spawns async tasks" do
-    pmap([1,2,3], fn x -> x * 2 end)
+  test "pmap maps a list", context do
+    assert context[:out_list] == pmap(context[:in_list], fn x -> x * 2 end)
+  end
+
+  test "pmap spawns async tasks", context do
+    pmap(context[:in_list], fn x -> x * 2 end)
 
     assert_received({pid1, :ok})
     assert_received({pid2, :ok})

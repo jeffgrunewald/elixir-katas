04b91bdf94222133d48b31949b947feae100cbbb
jeffgrunewald <jeff@grunewalddesign.com>
2017-10-07 23:20:31 -0400

    Failing on async message pass

--- hello_exunit/test/hello_exunit_test.exs
+++ hello_exunit/test/hello_exunit_test.exs
@@ -6,4 +6,14 @@ defmodule HelloExunitTest do
     assert [2,4,6] = pmap([1,2,3], fn x -> x * 2 end)
   end
 
+  test "pmap spawns async tasks" do
+    pmap([1,2,3], fn x -> x * 2 end)
+
+    assert_receive({pid1, 2})
+    assert_receive({pid2, 2})
+    assert_receive({pid3, 2})
+    refute pid1 == pid2
+    refute pid1 == pid3
+    refute pid2 == pid3
+  end
 end

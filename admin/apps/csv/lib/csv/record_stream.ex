647d7b7ba44de293c84783a4a2bcbf2f6f2cbeb2
jeffgrunewald <jeff@grunewalddesign.com>
2017-10-11 13:40:28 -0400

    Passing tests again

--- admin/apps/csv/lib/csv/record_stream.ex
+++ admin/apps/csv/lib/csv/record_stream.ex
@@ -1,9 +1,16 @@
 defmodule Csv.RecordStream do
-  def new(device, headers: _headers, schema: _schema) do
+  def new(device, headers: headers, schema: schema) do
     stream = device
     |> IO.stream(:line)
     |> Stream.map(&String.trim/1)
     |> Stream.map(&String.split(&1, ","))
+    |> Stream.map(fn(row) ->
+      contents = headers
+      |> Enum.zip(row)
+      |> Enum.into(%{})
+
+      struct(schema, contents)
+    end)
 
     _headers = stream |> Enum.fetch!(0)
 

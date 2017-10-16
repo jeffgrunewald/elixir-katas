f9001d33e2173d71ae332b853ccb50efdea5ccea
jeffgrunewald <jeff@grunewalddesign.com>
2017-10-15 23:11:43 -0400

    headers can now be out of order or missing

--- admin/apps/csv/lib/csv/record_stream.ex
+++ admin/apps/csv/lib/csv/record_stream.ex
@@ -1,19 +1,27 @@
 defmodule Csv.RecordStream do
-  def new(device, headers: headers, schema: schema) do
+  def new(device, headers: expected_headers, schema: schema) do
     stream = device
     |> IO.stream(:line)
     |> Stream.map(&String.trim/1)
     |> Stream.map(&String.split(&1, ","))
-    |> Stream.map(fn(row) ->
-      contents = headers
-      |> Enum.zip(row)
-      |> Enum.into(%{})
 
-      struct(schema, contents)
-    end)
+    headers = stream
+    |> Enum.fetch!(0)
+    |> Enum.map(&String.to_atom/1)
 
-    _headers = stream |> Enum.fetch!(0)
+    if Enum.sort(headers) == Enum.sort(expected_headers) do
+      structs = stream
+      |> Stream.map(fn(row) ->
+        contents = headers
+        |> Enum.zip(row)
+        |> Enum.into(%{})
 
-    {:ok, stream}
+        struct(schema, contents)
+      end)
+
+      {:ok, structs}
+    else
+      :invalid_csv
+    end
   end
 end
